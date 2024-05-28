7b74d29f96481027e011dc1b5e593e0df956b602
Ganesh-447 <grandhiganesh47@gmail.com>
2024-05-28 23:33:12 +0530

    28thMay

--- Programs/May 26 2024/prime.js
+++ Programs/May 26 2024/prime.js
@@ -1,8 +1,30 @@
-let a = [1,2,3,6,45,68,76,22,10];
-for(let i =0; i< a.length;i++)
+// let a = [1,2,3,6,45,68,76,22,10];
+// for(let i =0; i< a.length;i++)
+// {
+//     if (a[i]%2==0)
+//     {
+//         console.log(a[i])
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let b =[34,56,79,9,80];
+for (let i =0; i < b.length; i++)
 {
-    if (a[i]%2==0)
+    if(b[i]%2 == 0)
     {
-        console.log(a[i])
+        console.log(b[i])
     }
 }
